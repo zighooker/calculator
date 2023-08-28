79b2e1d76b8825d11996dc555e3b7c3727025d38
Zig Hooker <zighooker@gmail.com>
2023-08-27 23:02:44 -0700

    Built preliminary javascript

--- script.js
+++ script.js
@@ -1,25 +1,86 @@
-let firstNumber;
+let xNum = "0";
+let yNum = "0";
+let zNum = "0";
+let numSwitch = false;
 let operator;
-let secondNumber;
-let result;
+let result; 
 
-function operate(){
-    firstNumber = prompt("First number");
-    operator = prompt("Type operator");
-    secondNumber = prompt("Second number");
-    switch (operator) {
-        case "+":
-            result = +firstNumber + +secondNumber;
+const inputButtons = Array.from(document.getElementsByClassName('inputButton'));
+inputButtons.forEach(function (i) {
+    i.addEventListener('click', () => inputRouting(i.id));
+});
+const screen = document.getElementById('screen');
+
+function inputRouting(userInput) {
+    if (!numSwitch) inputProcess(userInput, xNum);
+    else inputProcess(userInput, yNum);
+};
+
+function inputProcess(userInput, currNum) {
+    if (userInput == 0 && currNum == 0) return;
+    if (!isNaN(userInput)) {
+        if (!zNum === "0") clear();
+        if (currNum === "0") currNum = userInput; 
+        else currNum += userInput;
+    }
+    switch (userInput) {
+        case "negative":
+            currNum = (+currNum * -1).toString();
             break;
-        case "-":
-            result = +firstNumber - +secondNumber;
+        case "clear":
+            currNum = "0";
+            clear();
             break;
-        case "*":
-            result = +firstNumber * +secondNumber;
-            break;
-        case "/":
-            result = +firstNumber / +secondNumber;
+        case "decimal":
+            if (!currNum.includes(".")) currNum += ".";
             break;
     }
-    alert(result);
+    if (!numSwitch) {
+        xNum = currNum
+        screen.innerText = xNum;
+    } else {
+        yNum = currNum
+        screen.innerText = yNum;
+    };
+    switch (userInput) {
+        case "÷":
+        case "x":
+        case "-":
+        case "+":
+            operator = userInput;
+            numSwitch = !numSwitch;
+            screen.innerText = operator;
+            break;
+        case "equals":
+            operate();
+    }
+}
+
+function clear() {
+    xNum = "0";
+    yNum = "0";
+    zNum = "0";
+    operator = "";
+}
+
+function operate() {
+    switch (operator) {
+        case "÷":
+            zNum = +xNum / +yNum;
+            break;
+        case "x":
+            zNum = +xNum * +yNum;
+            break;
+        case "-":
+            zNum = +xNum - +yNum;
+            break;
+        case "+":
+            zNum = +xNum + +yNum;
+            break;
+    }
+    numSwitch = !numSwitch;
+    if (zNum.length >= 12) zNum = parseFloat(zNum).toFixed(12);
+    zNum = zNum.toString();
+    xNum = zNum;
+    screen.innerText = zNum;
 }
