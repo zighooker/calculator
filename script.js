872c90920c29dacca27e0247c59fe691316624c4
Zig Hooker <zighooker@gmail.com>
2023-08-28 18:12:48 -0700

    Convert JS to function using arrays

--- script.js
+++ script.js
@@ -1,79 +1,104 @@
-let xNum = "0";
-let yNum = "0";
-let zNum = "0";
-let numSwitch = false;
-let operator;
-let result; 
+let operator = null;
+let numArray = ["0"];
+let displayResult = false;
 
 const inputButtons = Array.from(document.getElementsByClassName('inputButton'));
 inputButtons.forEach(function (i) {
     i.addEventListener('click', () => inputRouting(i.id));
-});
-const screen = document.getElementById('screen');
-const receiptPaper = document.querySelector('.receiptPaper');
+})
 
 function inputRouting(userInput) {
-    if (!numSwitch) inputProcess(userInput, xNum);
-    else inputProcess(userInput, yNum);
-};
+    // if (!numSwitch) inputProcess(userInput, xNum);
+    // else inputProcess(userInput, yNum);
+    if (!isNaN(userInput)) numberInput(userInput);
+    else {
+        switch (userInput) {
+            case "negative":
+                negative();
+                break;
+            case "clear":
+                clear();
+                break;
+            case "del":
+                del();
+                break;
+            case "decimal":
+                decimal();
+                break;
+            case "equals":
+                operate();
+                break;
+            default:
+                operator = userInput;
+                updateScreen(operator);
+                printOut(numArray[numArray.length - 1] +
+                    " " + operator);
+                numArray.push("0");
+        }
+    }
+}
 
-function inputProcess(userInput, currNum) {
-    if (userInput == 0 && currNum == 0) return;
-    if (!isNaN(userInput)) {
-        if (!zNum === "0") clear();
-        if (currNum === "0") currNum = userInput; 
-        else currNum += userInput;
+function numberInput(userInput) {
+    if (displayResult) clear();
+    let modNum = numArray.pop();
+    // won't accept input if multiple 0s or too long for display
+    if ((userInput === "0" && modNum === "0")
+            || modNum.length >= 12) {
+        numArray.push(modNum);
+        return;
     }
-    switch (userInput) {
-        case "negative":
-            currNum = (+currNum * -1).toString();
-            break;
-        // case "delete":
-        //     if (!numSwitch) xNum.slice(0, -1);
-        //     else yNum.slice(0, -1);
-        //     break;
-        case "clear":
-            currNum = "0";
-            clear();
-            break;
-        case "decimal":
-            if (!currNum.includes(".")) currNum += ".";
-            break;
-    }
-    if (!numSwitch) {
-        xNum = currNum
-        screen.innerText = xNum;
-    } else {
-        yNum = currNum
-        screen.innerText = yNum;
-    };
-    switch (userInput) {
-        case "÷":
-        case "x":
-        case "-":
-        case "+":
-            operator = userInput;
-            numSwitch = !numSwitch;
-            screen.innerText = operator;
-            break;
-        case "equals":
-            operate();
-    }
+    // replaces 0 if first digit of number
+    if (modNum === "0") modNum = userInput;
+    else modNum += userInput;
+    numArray.push(modNum);
+    updateScreen();
+}
+
+function negative() {
+    let modNum = numArray.pop();
+    modNum = (+modNum * -1).toString();
+    numArray.push(modNum);
+    updateScreen();
 }
 
 function clear() {
-    xNum = "0";
-    yNum = "0";
-    zNum = "0";
-    operator = "";
+    numArray = ["0"];
+    operator = null;
+    displayResult = false;
+    printOut(`\xa0`)
+    updateScreen();
+}
+
+function del() {
+    let modNum = numArray.pop();
+    if (modNum.length >= 2) modNum = modNum.slice(0, -1);
+    else modNum = "0";
+    numArray.push(modNum);
+    updateScreen();
+}
+
+function decimal() {
+    let modNum = numArray.pop();
+    if (!modNum.includes(".")) modNum += ".";
+    numArray.push(modNum);
+    updateScreen();
 }
 
 function operate() {
+    xNum = numArray[numArray.length - 2];
+    yNum = numArray[numArray.length - 1];
+    printOut(yNum + " =");
+    if (xNum === undefined || yNum === undefined 
+            || operator === null) {
+        updateScreen("ERROR");
+        return;
+    }
+    let zNum;
     switch (operator) {
         case "÷":
             zNum = +xNum / +yNum;
             break;
-        case "x":
+        case "*":
             zNum = +xNum * +yNum;
             break;
         case "-":
@@ -83,19 +108,26 @@ function operate() {
             zNum = +xNum + +yNum;
             break;
     }
-    numSwitch = !numSwitch;
     if (zNum.length >= 12) zNum = parseFloat(zNum).toFixed(12);
     zNum = zNum.toString();
-    xNum = zNum;
-    screen.innerText = zNum;
-    printOut(yNum);
+    numArray.splice(-1, 0, zNum);
+    displayResult = true;
+    updateScreen(zNum);
+    setTimeout(() => {printOut(zNum);}, 250);
 }
 
+const screen = document.getElementById('screen');
+function updateScreen(displayText) {
+    if (!displayText) screen.innerText = numArray[numArray.length - 1];
+    else screen.innerText = displayText;
+}
 
+const receiptPaper = document.querySelector('.receiptPaper');
 function printOut(printText){
     const newLine = document.createElement('p');
     newLine.innerText = printText;
     receiptPaper.appendChild(newLine);
     let receiptHeight = receiptPaper.offsetHeight + 26;
     receiptPaper.setAttribute('style', `height: ${receiptHeight}px`);
-}
+};
+
