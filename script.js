ad37f7697c1b4d7d435176953aa7ef42d5b6eba6
Zig Hooker <zighooker@gmail.com>
2023-08-29 18:34:23 -0700

    Add additional condition handling to functions Remove transition delay from receipt to avoid issues

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let currVal = "0";
 let operator = null;
 let xOperand = null;
 let operandInput = false;
+let secondInput = false;
 let resultShown = false;
 
 // sets up buttons with listeners
@@ -16,17 +17,24 @@ function inputRouting(userInput) {
     else {
         switch (userInput) {
             case "negative":
+                if (resultShown) clear();
                 negative();
+                break;
+            case "decimal":
+                if (resultShown) clear();
+                decimal();
+                break;
+            case "del":
+                if (resultShown) clear();
+                del();
                 break;
             case "clear":
                 clear();
                 break;
-            case "del":
-                del();
+            case "eq":
+                executeOperator();
                 break;
-            case "decimal":
-                decimal();
-                break;
+            // all operator inputs are handled here
             default:
                 setOperator(userInput);
                 break;
@@ -35,47 +43,64 @@ function inputRouting(userInput) {
 }
 
 function numberInput(userInput) {
+    // clears if a previous calculation is still showing
     if (resultShown) {
         clear();
         currVal = userInput;
+    // switches over to second operand
     } else if (operandInput) {
         currVal = userInput;
+        secondInput = true;
         operandInput = false;
+    // forbids multiple 0s at start or too many digits for display
     } else if ((userInput === "0" && currVal === "0")
-        || currVal.length >= 12) {
+        || currVal.length >= 10) {
             return;
+    // replaces default 0 with user input
     } else if (currVal === "0") {
         currVal = userInput;
+    // appends user input onto string
     } else {
         currVal += userInput;
     }
     updateScreen();
 }
 
-// checks if most recent array entry is number
-// if so multiplies by -1
+// multiplies current operand by -1
 function negative() {
     currVal = (+currVal * -1).toString();
     updateScreen();
 }
 
+//  resets all variables and booleans
 function clear() {
     currVal = "0";
     xOperand = null;
     operator = null;
     operandInput = false;
+    secondInput = false;
     resultShown = false;
     printOut(`\xa0`)
     updateScreen();
 }
 
 function del() {
+    // lets user delete operator input without affecting operand
+    if (operator && !secondInput) {
+        operator = null;
+        operandInput = false;
+        xOperand = null;
+        updateScreen();
+        return;
+    }
+    // cuts the last digit off current number
     if (currVal.length >= 2) currVal = currVal.slice(0, -1);
     else currVal = "0";
     updateScreen();
 }
 
 function decimal() {
+    // lets users start an operand with .
     if (operandInput) {
         currVal = "0.";
         operandInput = false;
@@ -84,42 +109,50 @@ function decimal() {
         clear();
         currVal = "0.";
     }
+    // forbids more than one . per operand
     if (!currVal.includes(".")) currVal += ".";
     updateScreen();
 }
 
 function setOperator(userInput) {
-    if (operator && operandInput && userInput !== "eq") {
+    // lets user change operator
+    if (operator && operandInput) {
         operator = userInput;
-        updateScreen(userInput);
+        updateScreen(`${currVal}${userInput}`);
         return;
     }
 
-    // if (userInput !== "=") 
-    updateScreen(userInput);
-    // printOut(`${currVal} ${userInput}`);
+    updateScreen(`${currVal}${userInput}`);
     
-    if (xOperand === null) xOperand = currVal;
+    // if no operand is set, assigns currVal to xOperand
+    if (xOperand === null) {
+        xOperand = currVal;
+        printOut(xOperand);
+    }
     else if (operator) executeOperator();
 
+    // assigns input as operator
     operandInput = true;
-    if (userInput !== "eq") operator = userInput;
+    operator = userInput;
 }
 
 function executeOperator() {
-    printOut(`${currVal} ${operator}=`);
+    if (operator) printOut(`${currVal} ${operator}=`);
+    else printOut(`${currVal} =`);
+
     let resultVal = calculate(xOperand, currVal, operator);
 
-    // checks if result is over max length of display
+    // shortens result if over max display length
     resultVal = parseFloat(resultVal.toFixed(8));
     if (resultVal.toString().length >= 12) 
         resultVal = resultVal.toExponential(6);
-    resultVal = resultVal.toString()
 
+    
+    resultVal = resultVal.toString();
     xOperand = resultVal;
     resultShown = true;
     updateScreen(resultVal);
-    setTimeout(() => {printOut(resultVal);}, 250);
+    printOut(resultVal);
 }
 
 function calculate(x, y, operator) {
@@ -127,16 +160,17 @@ function calculate(x, y, operator) {
     if (operator === "*") return +x * +y;
     if (operator === "-") return +x - +y;
     if (operator === "+") return +x + +y;
-    if (operator === "eq") return +y;
+    if (operator === "eq" || !operator) return +y;
 }
 
+// updates display text html
 const screen = document.getElementById('screen');
 function updateScreen(displayText) {
     if (!displayText) screen.innerText = currVal;
     else screen.innerText = displayText;
 }
 
-// increments height of receipt paper and adds new p element
+// adds new p element to html and increments height of receipt element
 const receiptPaper = document.querySelector('.receiptPaper');
 function printOut(printText){
     const newLine = document.createElement('p');
@@ -146,6 +180,7 @@ function printOut(printText){
     receiptPaper.setAttribute('style', `height: ${receiptHeight}px`);
 };
 
+// optional keyboard input listeners
 window.addEventListener("keydown", function(e) {
     if (e.code === "Backspace") inputRouting("del");
     if (e.code === "KeyC") inputRouting("clear");
